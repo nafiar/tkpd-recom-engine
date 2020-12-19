3f2dd15f3fa1b4eb33c2b3a15106f53ceef9db56
nafiarr <nafiar21@gmail.com>
2020-12-19 12:42:09 +0700

    update http init

--- internal/serving/repository/similarproduct/http/init.go
+++ internal/serving/repository/similarproduct/http/init.go
@@ -1,10 +1,17 @@
 package http
 
-import "github.com/nafiar/tkpd-recom-engine/internal/serving/repository/similarproduct"
+import (
+	"net/http"
+
+	"github.com/nafiar/tkpd-recom-engine/internal/serving/repository/similarproduct"
+)
 
 type httpSimilarProduct struct {
+	client http.Client
 }
 
 func New() similarproduct.Repository {
-	return &httpSimilarProduct{}
+	return &httpSimilarProduct{
+		client: http.Client{},
+	}
 }
